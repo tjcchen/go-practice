af95b489abae23c00540817264fe3564d59fed82
Andy Chen <chenyangpoppin@hotmail.com>
2023-04-24 20:28:50 +0800

    feat: for loop

---
+++ forloop/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 0; i < 3; i++ {
+		fmt.Println(i)
+	}
+
+	fullString := "hello world"
+	fmt.Println(fullString)
+	
+	for i, c := range fullString {
+		fmt.Println(i, string(c))
+	}
+}
