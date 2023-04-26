181dce9d2043b4a71a660fd2b4234217357b947f
Andy Chen <chenyangpoppin@hotmail.com>
2023-04-26 20:55:49 +0800

    feat: variadic function parameters

--- helloworld/main.go
+++ helloworld/main.go
@@ -27,6 +27,26 @@ func main() {
 	} else {
 		fmt.Println(err)
 	}
+
+	// variadic parameters
+	// Eg: func append(slice []Type, elems ...Type) []Type
+	arr := append([]int{1, 2}, 3, 4, 5)
+	fmt.Println(arr) // [1 2 3 4 5]
+
+	myArr := []string{}
+	myArr = append(myArr, "a", "b", "c")
+	fmt.Println(myArr) // [a b c]
+
+	/**
+	 * Some useful built-in functions:
+	 * 1. close - close pipeline
+	 * 2. len, cap - returns the length and capacity of array, slice, or map
+	 * 3. new, make - memory allocation
+	 * 4. copy, append - manipulate with slice
+	 * 5. panic, recover - error catching mechanism
+	 * 6. print, println - print messages
+	 * 7. complex, real, imag
+	 */
 }
 
 /**
@@ -37,4 +57,16 @@ func DuplicateString(input string) (error, string) {
 		return fmt.Errorf("aaa is not allowed"), ""
 	}
 	return nil, input + input
+}
+
+/**
+ * give return value a name, in this case, err & result
+ */
+func DuplicateString1(input string) (err error, result string) {
+	if input == "aaa" {
+		err = fmt.Errorf("aaa is not allowed")
+		return;
+	}
+	result = input + input
+	return;
 }
--- init/main.go
+++ init/main.go
@@ -10,6 +10,7 @@ import (
  * 1. init function will be executed when package is initiated
  * 2. init is executed before main function
  * 3. be cautious to use init() when we have nested imports
+ * 4. init() method in each package is executed once only
  */
 func init() {
 	fmt.Println("main init")
