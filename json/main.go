97d3af7e2b95adfb236b4c60342974ec00991907
Andy Chen <chenyangpoppin@hotmail.com>
2023-04-29 14:25:46 +0800

    feat: json decode

--- json/main.go
+++ json/main.go
@@ -7,7 +7,7 @@ import (
 
 type Human struct {
 	Name string
-	Age int
+	Age  int
 }
 
 // Unmarshal: convert string to struct
@@ -22,7 +22,7 @@ func unmarshal2Struct(humanStr string) Human {
 
 // Marshal: convert struct to string
 func marshal2JsonString(h Human) string {
-	h.Age = 30                // change age internally
+	h.Age = 30 // change age internally
 	updatedBytes, err := json.Marshal(&h)
 	if err != nil {
 		println(err)
@@ -31,17 +31,58 @@ func marshal2JsonString(h Human) string {
 }
 
 func main() {
-	fmt.Println("---json marshal and unmarshal---")
+	fmt.Println("---json marshal and unmarshal starts---")
 
 	human := new(Human)
 	human.Name = "Andy Chen"
 	human.Age = 28
 
 	jsonStr := marshal2JsonString(*human)
-	fmt.Println(jsonStr)       // {"Name":"Andy Chen","Age":30}
+	fmt.Println(jsonStr) // {"Name":"Andy Chen","Age":30}
 
 	hStruct := unmarshal2Struct(jsonStr)
-	fmt.Println(hStruct)       // {Andy Chen 30}
-	fmt.Println(hStruct.Name)  // Andy Chen
-	fmt.Println(hStruct.Age)   // 30
+	fmt.Println(hStruct)      // {Andy Chen 30}
+	fmt.Println(hStruct.Name) // Andy Chen
+	fmt.Println(hStruct.Age)  // 30
+
+	fmt.Println("---json marshal and unmarshal ends---")
+
+	// json decode example
+	err, _ := jsonDecode(jsonStr)
+	if err != nil {
+		fmt.Println("failed")
+	} else {
+		fmt.Println("succeed")
+	}
+}
+
+func jsonDecode(jsonStr string) (error, bool) {
+	fmt.Println("\n---json decode starts---")
+	var obj interface{}                          // interface{} is a base type
+	err := json.Unmarshal([]byte(jsonStr), &obj) // after this, jsonStr becomes a map
+
+	if err != nil {
+		return fmt.Errorf("an error occured when unmarshaled json string"), false
+	}
+
+	fmt.Println("sss", obj) // sss map[Age:30 Name:Andy Chen]
+
+	objMap, ok := obj.(map[string]interface{})
+
+	if !ok {
+		return fmt.Errorf("an error occured when unmarshaled json string"), false
+	}
+
+	for k, v := range objMap {
+		switch value := v.(type) {
+		case string:
+			fmt.Printf("type of %s is string, value is %v\n", k, value) // type of Name is string, value is Andy Chen
+		case interface{}:
+			fmt.Printf("type of %s is interface{}, value is %v\n", k, value) // type of Age is interface{}, value is 30
+		default:
+			fmt.Printf("type of %s is wrong, value is %v\n", k, value)
+		}
+	}
+	fmt.Println("---json decode ends---")
+	return nil, true
 }
