3d4b4a3848340fd25278d85d676a91dafa72a55a
Andy Chen <chenyangpoppin@hotmail.com>
2023-04-30 22:21:56 +0800

    feat: adding a panic & recover example

---
+++ panic/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// panic: occurs when there is a system error, panic will make current thread crash
+	// defer: transfer the control to the panic caller
+	// recover: function recover from the panic or system errors
+
+	// execution order:
+	// 1. defer func is called
+	// 2. a panic is triggered
+	defer func() {
+		fmt.Println("defer func is called")
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	panic("a panic is triggered")
+}
