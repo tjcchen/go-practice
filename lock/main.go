062f0611ce351a50c09eb397469b188062d8fe5a
Andy Chen <chenyangpoppin@hotmail.com>
2023-05-06 19:14:51 +0800

    chore: memory management

--- lock/main.go
+++ lock/main.go
@@ -9,11 +9,11 @@ func main() {
 	// - thread safety( multi-thread code ): thread-safe code only manipulates shared data structures in a manner
 	//   that ensures that all threads behave properly and fulfill their design specifications
 	//   without unintended interaction.
-	// - Go not only support CSP(Communicating Sequential Process) model, the communcation between different channels,
+	// - Golang not only support CSP(Communicating Sequential Process) model, the communcation between different channels,
 	//   but also support multi-thread data sharing
 	// - Sync syntax:
 	//   sync.Mutex( mutual exclusion ) - mutual exclusion: Lock() add lock; UnLock() remove lock
-	//   sync.RWMutex - read and write seperately
+	//   sync.RWMutex - read and write seperately: read does not block thread, but write blocks.
 	//   sync.WaitGroup - wait for a group of goroutine returns
 	//   sync.Once - code being executed once only
 	//   sync.Cond - make a group of goroutine being revoked under certain conditions
@@ -31,6 +31,28 @@ func main() {
 	// - linux page size: getconf PAGE_SIZE( 4096 )
 	// - GoLang implements user thread based on GMP( Goroutine, Machine, Process ) model
 
-	fmt.Println("---thread lock---")
+	// 3. Memory management
+	// Heap memory management:
+	// - Mutator( Ask for memory from Allocator ),
+	// - Allocator( apply for a big memory block once, like 10G. used & unused ),
+	// - Heap,
+	// - Object Header( object header + metadata ),
+	// - Collector( Garabage Collector, scan all the object headers )
+	// ThreadCacheMalloc( Heap memory pre-allocation, each thread maintains its own private memory space to avoid multi-threads competition ):
+	// - ThreadCache
+	// - CentralCache
+	// - PageHeap
+	// - Virtual Memory
+  // Golang memory allocation( two span class matches to one size class, one for pointer, the other for reference ):
+	// - mcache
+	// - mcentral
+	// - mheap
+	// - virtual memory
+	// Memory collection:
+	// - reference counter( Python, PHP, Swift ): each memory block maintains a counter, the an object is released, the counter--
+	// - mark & sweep( Golang ): scan code from root, mark reference object, and collect unmarked object
+	// - generation collector( Java ): Eden, Survivor, Tenured
+
+	fmt.Println("---Go Routine---")
 
 }
