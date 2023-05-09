73e1d23ee98ffe2255d04e09fe363abd2fca17ef
Andy Chen <chenyangpoppin@hotmail.com>
2023-05-09 17:42:04 +0800

    chore: comment for dlv

--- govet/main.go
+++ govet/main.go
@@ -5,9 +5,10 @@ import (
 )
 
 func main() {
+	// dlv: Delve is a debugger for the Go programming language.
 	name := "testing"
-	fmt.Printf("%d\n", name)         // fmt.Printf format %d has arg name of wrong type string
-	fmt.Printf("%s\n", name, name)   // fmt.Printf call needs 1 arg but has 2 args
+	fmt.Printf("%d\n", name)       // fmt.Printf format %d has arg name of wrong type string
+	fmt.Printf("%s\n", name, name) // fmt.Printf call needs 1 arg but has 2 args
 
 	fmt.Printf("%s\n", name)         // correct
 	fmt.Printf("%s%s\n", name, name) // correct
